8f0b7ee7fc1cc73aca34cc1d212794b0d3dd3278
MarioLucen <mlucena@islautopia.com>
2018-09-24 14:11:18 +0200

    conection with API json

--- es6/main.js
+++ es6/main.js
@@ -1,4 +1,30 @@
-console.log('hola');
+
+const postsContainer = document.querySelector('.container-post')
+
+const postTemplate = (post) => `<div class="article">
+    <div class="text-article">
+    <p>${post.title}</p>
+    <p class="read">${post.body}</p>
+    <div class="read-information">
+        <p>Mac DeGeurin</p>
+        <i class="im im-star"></i>
+        <p class="read">4 minutes read</p>
+        <i class="im im-bookmark"></i>
+    </div>
+    </div>
+    <img class="img-article" src="img/camara.jpeg" alt="camara">
+</div>`
+
 fetch('https://jsonplaceholder.typicode.com/posts/')
 .then(response => response.json())
-.then(json => console.log(json))
+.then(json => {
+    const posts = json.map((post) => postTemplate(post))
+    console.log(postsContainer)
+    postsContainer.innerHTML = posts.join('')
+})
+
+// function sum (a,b) {
+//    return a + b 
+// } 
+
+// const sum = (a,b) => a + b
--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 'use strict';
 
-console.log('hola');
+var postsContainer = document.querySelector('.container-post');
+
+var postTemplate = function postTemplate(post) {
+    return '<div class="article">\n    <div class="text-article">\n    <p>' + post.title + '</p>\n    <p class="read">' + post.body + '</p>\n    <div class="read-information">\n        <p>Mac DeGeurin</p>\n        <i class="im im-star"></i>\n        <p class="read">4 minutes read</p>\n        <i class="im im-bookmark"></i>\n    </div>\n    </div>\n    <img class="img-article" src="img/camara.jpeg" alt="camara">\n</div>';
+};
+
 fetch('https://jsonplaceholder.typicode.com/posts/').then(function (response) {
-  return response.json();
+    return response.json();
 }).then(function (json) {
-  return console.log(json);
+    var posts = json.map(function (post) {
+        return postTemplate(post);
+    });
+    console.log(postsContainer);
+    postsContainer.innerHTML = posts.join('');
 });
+
+// function sum (a,b) {
+//    return a + b 
+// } 
+
+// const sum = (a,b) => a + b
