9ad9382779ed9e1e05a3df43187b962de2bb96e6
MarioLucen <mlucena@islautopia.com>
2018-09-24 11:10:46 +0200

    finished marked

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-"use strict";
+'use strict';
+
+console.log('hola');
+fetch('https://jsonplaceholder.typicode.com/posts/').then(function (response) {
+  return response.json();
+}).then(function (json) {
+  return console.log(json);
+});
